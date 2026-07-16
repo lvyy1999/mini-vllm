fix some precision bug

src/minivllm/layers/rmsnorm.py
@@ -1,6 +1,7 @@
 import torch
 
 
+# RMSNorm(x) = (x / sqrt(mean(x²) + ε)) ⊙ γ
 class RMSNorm(torch.nn.Module):
 
     def __init__(self, hidden_size: int, eps: float = 1e-6):
@@ -11,22 +12,29 @@ class RMSNorm(torch.nn.Module):
 
     @torch.compile
     def rms_forward(self, x: torch.Tensor) -> torch.Tensor:
-        # RMSNorm(x) = (x / sqrt(mean(x²) + ε)) ⊙ γ
-        rms = torch.sqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
-        x_norm = x / rms * self.weight
-        return x_norm
+        origin_dtype = x.dtype
+        x = x.float()
+        var = x.pow(2).mean(dim=-1, keepdim=True)
+        x.mul_(torch.rsqrt(var + self.eps))
+        x = x.to(origin_dtype).mul_(self.weight)
+        return x
 
     @torch.compile
     def residual_rms_forward(
         self, x: torch.Tensor, residual: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        x = x + residual
-        return self.rms_forward(x), x
+        origin_dtype = x.dtype
+        x = x.float().add_(residual.float())
+        residual = x.to(origin_dtype)
+        var = x.pow(2).mean(dim=-1, keepdim=True)
+        x.mul_(torch.rsqrt(var + self.eps))
+        x = x.to(origin_dtype).mul_(self.weight)
+        return x, residual
 
     def forward(
         self, x: torch.Tensor, residual: torch.Tensor | None = None
     ) -> tuple[torch.Tensor, torch.Tensor] | torch.Tensor:
-        if residual is not None:
-            return self.residual_rms_forward(x, residual)
-        else:
+        if residual is None:
             return self.rms_forward(x)
+        else:
+            return self.residual_rms_forward(x, residual)

src/minivllm/layers/rotary_embedding.py
@@ -6,16 +6,10 @@ import torch.nn as nn
 def apply_rope_adjacent(
     x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor
 ) -> torch.Tensor:
-    if x.dim() == 3:  # (total_tokens, num_heads, head_dim)
-        # cos, sin shape: (seq_len, head_dim/2) -> (seq_len, 1, head_dim/2)
-        cos = cos.unsqueeze(1)
-        sin = sin.unsqueeze(1)
-    else:  # (B, seq_len, num_heads, head_dim)
-        # cos, sin shape: (seq_len, head_dim/2) -> (1, seq_len, 1, head_dim/2)
-        cos = cos.unsqueeze(0).unsqueeze(2)
-        sin = sin.unsqueeze(0).unsqueeze(2)
     # Split x into two halves by interleaved mode
     # x0, x2, ...
+    origin_dtype = x.dtype
+    x = x.float()
     x_even = x[..., ::2]
     # x1, x3, ...
     x_odd = x[..., 1::2]
@@ -25,29 +19,22 @@ def apply_rope_adjacent(
     y_even = x_even * cos - x_odd * sin
     y_odd = x_odd * cos + x_even * sin
     y = torch.zeros_like(x)
-    y[:, ::2] = y_even
-    y[:, 1::2] = y_odd
-    return y
+    y[..., ::2] = y_even
+    y[..., 1::2] = y_odd
+    return y.to(origin_dtype)
 
 
 # apply rope between two elements at a distance of head_dim / 2
 def apply_rotary_pos_emb(
     x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor
 ) -> torch.Tensor:
-    if x.dim() == 3:  # (total_tokens, num_heads, head_dim)
-        # cos, sin shape: (seq_len, head_dim/2) -> (seq_len, 1, head_dim/2)
-        cos = cos.unsqueeze(1)
-        sin = sin.unsqueeze(1)
-    else:  # (B, seq_len, num_heads, head_dim)
-        # cos, sin shape: (seq_len, head_dim/2) -> (1, seq_len, 1, head_dim/2)
-        cos = cos.unsqueeze(0).unsqueeze(2)
-        sin = sin.unsqueeze(0).unsqueeze(2)
     # Split x into two halves along the head dimension
-    x1, x2 = x.chunk(2, dim=-1)
+    origin_dtype = x.dtype
+    x1, x2 = x.float().chunk(2, dim=-1)
     # Apply rotary embedding with proper broadcasting
     y1 = x1 * cos - x2 * sin
     y2 = x1 * sin + x2 * cos
-    return torch.cat([y1, y2], dim=-1).to(x.dtype)
+    return torch.cat([y1, y2], dim=-1).to(origin_dtype)
 
 
 class RotaryEmbedding(nn.Module):
@@ -67,7 +54,7 @@ class RotaryEmbedding(nn.Module):
         super().__init__()
 
         inv_freq = 1.0 / (
-            base ** (torch.arange(0, head_dim, 2) / head_dim)
+            base ** (torch.arange(0, head_dim, 2, dtype=torch.float) / head_dim)
         )  # shape(head_dim/2, )
 
         if is_llama3:
@@ -94,11 +81,11 @@ class RotaryEmbedding(nn.Module):
                 factor = (1 - smooth) / llama3_rope_factor + smooth
                 inv_freq = factor * inv_freq
 
-        positions = torch.arange(max_position).float()  # shape(max_seq_len, )
+        positions = torch.arange(max_position, dtype=torch.float)  # shape(max_seq_len, )
         angles = torch.outer(positions, inv_freq)  # shape(max_seq_len, head_dim/2)
         cos = torch.cos(angles)  # shape(max_seq_len, head_dim/2)
         sin = torch.sin(angles)  # shape(max_seq_len, head_dim/2)
-        cos_sin_cache = torch.cat([cos, sin], dim=-1)  # shape(max_seq_len, head_dim)
+        cos_sin_cache = torch.cat([cos, sin], dim=-1).unsqueeze(1)  # shape(max_seq_len, 1, head_dim)
         self.register_buffer("cos_sin_cache", cos_sin_cache)
 
     @torch.compile

src/minivllm/layers/sampler.py
@@ -13,7 +13,7 @@ class SamplerLayer(nn.Module):
         logits: torch.Tensor,  # shape(batch_size, vocab_size)
         temperature: torch.Tensor,  # shape(batch_size, )
     ) -> torch.Tensor:
-        logits /= temperature.unsqueeze(-1)
+        logits = logits.float() / temperature.unsqueeze(-1)
         probs = torch.softmax(logits, dim=-1)
         sample_tokens = probs.div_(
             torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)